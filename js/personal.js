7520bd9e5ac9ca812333eab249020092a0050a52
caldwell619 <caldwell8814@gmail.com>
2018-12-10 23:07:02 -0600

    Lots of progress

--- js/personal.js
+++ js/personal.js
@@ -3,4 +3,47 @@ $('.mobile-menu-icon').on( "click", function (){
   $('.collapsible-nav-links').slideToggle()
 });
 
+//$('.html-dropdown')
+
+//targets entire card
+//need to figure out how to slow transition to center
+var dropDowns = [$("#html-card"), $("#css-card"), $("#js-card"), $('#react-card')];
+
+//leaves card
+// var dropDowns = [$(".html-dropdown"), $(".css-dropdown"), $(".js-dropdown"), $('.react-dropdown')];
+
+//HTML toggle
+dropDowns[0].on("click", function() {
+    for (var i = 1; i < dropDowns.length; i++) {
+        dropDowns[i].fadeToggle();
+    }
+    $(".html-text").slideToggle();
+});
+
+//CSS Toggle
+dropDowns[1].on("click", function(){
+    dropDowns[0].fadeToggle();
+    dropDowns[2].fadeToggle();
+    dropDowns[3].fadeToggle();
+    $(".css-text").slideToggle();
+});
+
+//JS Toggle
+dropDowns[2].on("click", function(){
+    dropDowns[0].fadeToggle();
+    dropDowns[1].fadeToggle();
+    dropDowns[3].fadeToggle();
+    $(".js-text").slideToggle();
+});
+
+//React Toggle
+$('.react-dropdown').on("click", function(){
+    dropDowns[0].fadeToggle();
+    dropDowns[1].fadeToggle();
+    dropDowns[2].fadeToggle();
+    $(".react-text").slideToggle();
+});
+
+
+
 
