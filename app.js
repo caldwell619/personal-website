19189bb009a6b71ce08f0e6884bcf449adb7f26c
caldwell619 <caldwell8814@gmail.com>
2019-02-20 17:07:15 -0600

    fixed small rendering bugs and css

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => {
 });
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/images", express.static(__dirname + '/images'));
+app.use("/js", express.static(__dirname + '/js'));
 
 
 const PORT = process.env.PORT || 3000;
